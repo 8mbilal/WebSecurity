fc32110cd454b82b6e1b1ebb64e966259969ef22
Muhammad Bilal <115345261+Muhammad-Bilal-456@users.noreply.github.com>
2024-08-17 22:21:33 +0500

    Add files via upload

---
+++ DirectoryTraversalAttacks/DirectoryTraversalAttacks/wwwroot/files/program.cs
@@ -0,0 +1,11 @@
+// wwwroot/Program.cs
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("This is a test file located inside the wwwroot directory.");
+    }
+}
